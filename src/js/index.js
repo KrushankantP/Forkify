b4fc06f25858d15d4668b1ec71775ab2e720e43a
Patel Krushankant <patelkk1618@gmail.com>
2020-09-23 17:36:24 -0500

    first

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+// Global app controller
+
+import {num,str} from './test';
+
+
+console.log(`I imported ${num} ${str} from another module`);
---
+++ src/js/test.js
@@ -0,0 +1,6 @@
+console.log('Imported Module');
+let num = 55;
+let str = "arshan";
+export {num,str};
+
+//export default 23;

4b04a882691bce94590cbb0ee42978a3406789a8
Patel Krushankant <patelkk1618@gmail.com>
2020-09-26 02:30:07 -0500

    update recipe servings

--- src/js/index.js
+++ src/js/index.js
@@ -114,3 +114,23 @@ const controlRecipe = async ()=>{
 
 //above two line of code into one line of code.
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
+
+
+//Handling recipe button clicks
+
+elements.recipe.addEventListener('click', e=>{
+    if(e.target.matches('.btn-decrease, .btn-decrease *')){
+        //Decrease button is clicked
+        if(state.recipe.servings > 1){
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe)
+        }
+
+
+    }else if(e.target.matches('.btn-increase, .btn-increase *')){
+        //Increase button is clicked
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe)
+    }
+    console.log(state.recipe)
+})
